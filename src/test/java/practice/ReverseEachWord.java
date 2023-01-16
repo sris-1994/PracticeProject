f71e22d32aa23f527fef882d47b8861f0f4f5664
sxp292 <srishti.panda@sherwin.com>
2023-01-16 19:46:17 +0530

    Pushing code for reverse each word of string

--- src/test/java/practice/ReverseEachWord.java
+++ src/test/java/practice/ReverseEachWord.java
@@ -1,4 +1,24 @@
 package practice;
 
+
 public class ReverseEachWord {
-}
+        public static void reverWordOfString(String inputString){
+            String words[] = inputString.split(" ");
+            String reverseString = "";
+            for (int i =0; i<words.length; i++){
+                String word = words[i];
+                String reverseWord = "";
+                for(int j = word.length()-1; j>=0; j--){
+                        reverseWord = reverseWord + word.charAt(j);
+                }
+                reverseString = reverseString + reverseWord + " ";
+            }
+            System.out.println(inputString);
+            System.out.println(reverseString);
+
+        }
+
+    public static void main(String[] args) {
+        reverWordOfString("Reverse each word of string");
+    }
+    }
