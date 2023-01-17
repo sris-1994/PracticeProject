18f5025b9d57282d59faece7c71dbbdfacd78736
sxp292 <srishti.panda@sherwin.com>
2023-01-17 13:20:46 +0530

    Pushing code for reverse string

--- src/test/java/practice/ReverseString.java
+++ src/test/java/practice/ReverseString.java
@@ -2,7 +2,7 @@ package practice;
 //reverse string
 public class ReverseString {
     public static void main(String[] args) {
-        String str = "I am a girl", rstr = "";
+        String str = "Java concept", rstr = "";
         for (int i=str.length()-1; i>=0; i--){
             rstr = rstr+str.charAt(i);
         }
