ddd703288a16b95f00e6b4b46e462378cb3f33f6
srish <srishtisimona@gmail.com>
2024-04-01 16:49:17 +0530

    Adding code to print a pattern

--- src/test/java/practice/Patterns.java
+++ src/test/java/practice/Patterns.java
@@ -1,6 +1,6 @@
 package practice;
 
-    public class Pattern {
+    public class Patterns {
         public static void main(String[] args) {
             int n = 5;
             for (int i = 0; i<n; i++){
