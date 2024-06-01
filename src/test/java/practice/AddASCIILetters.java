0fe3d625cdd586ff4f541689637d7a8bfd95d5a1
srish <srishtisimona@gmail.com>
2024-06-01 16:21:08 +0530

    Adding code to add ascii letters.

---
+++ src/test/java/practice/AddASCIILetters.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class AddASCIILetters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String inputStr = sc.nextLine();
+        int sum = 0;
+        for(int i = 0; i<inputStr.length(); i++){
+            sum+= inputStr.charAt(i);
+        }
+        System.out.println("The Ascii value sum of " +inputStr+ " is " +sum);
+    }
+}
